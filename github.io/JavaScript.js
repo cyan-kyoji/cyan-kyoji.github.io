9376a9ab2c43ab4079bb1fd7fa5d32395aee40bf
cyan-kyoji <cyan.kyoji@gmail.com>
2024-06-07 12:04:02 +0900

    Delete github.io directory

--- github.io/JavaScript.js
+++
@@ -1,9 +0,0 @@
-﻿// Smooth scroll for navigation links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
